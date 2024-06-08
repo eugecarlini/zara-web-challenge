a15de93b6133f2ec93ee2c0e81d7c689a62987db
Eugenia Carlini <eugeniacarlini@gmail.com>
2024-06-08 12:42:47 +0200

    feat: implement context into views

--- src/pages/CharacterDetail/index.tsx
+++ src/pages/CharacterDetail/index.tsx
@@ -1,9 +1,76 @@
-import React from "react";
+import React, { useEffect, useMemo } from "react";
 import { useParams } from "react-router-dom";
+import { useCharacters } from "@/context/CharacterContext";
 
 const CharacterDetail: React.FC = () => {
   const { id } = useParams<{ id: string | undefined }>();
-  return <article>Character {id}</article>;
+  const { characters, comics, fetchComicsByCharacter, loading, error } =
+    useCharacters();
+
+  const characterId = useMemo(() => Number(id), [id]);
+
+  useEffect(() => {
+    if (!isNaN(characterId)) {
+      fetchComicsByCharacter(id!);
+    }
+  }, [characterId, fetchComicsByCharacter, id]);
+
+  useEffect(() => {
+    // If context api state is not undefined
+    if (characterId && characters.length !== 0) {
+      fetchComicsByCharacter(characterId.toString());
+    }
+  }, [id, fetchComicsByCharacter]);
+
+  const characterById = useMemo(
+    () => characters.find((character) => character.id === Number(id)),
+    [characters, characterId]
+  );
+
+  if (!characterId) {
+    return <div>Character not found</div>;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error fetching character detail</div>;
+  }
+
+  const characterComics = comics[characterId];
+
+  return (
+    <article>
+      {characters.length ? (
+        <>
+          <h1>{characterById?.name}</h1>
+          <p>{characterById?.description}</p>
+          <img
+            src={characterById?.imageSrc}
+            alt={characterById?.name}
+            width="150"
+            height="150"
+          />
+        </>
+      ) : (
+        <p>Character not found</p>
+      )}
+
+      {characterComics && (
+        <ul>
+          {characterComics.map(({ id, name, imageSrc, year }) => (
+            <li key={id}>
+              <p>{name}</p>
+              <img src={imageSrc} alt={name} width="100" height="100" />
+              <p>Year: {year}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </article>
+  );
 };
 
 export default CharacterDetail;
