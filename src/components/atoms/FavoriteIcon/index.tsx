6768aef1c8da30011dbe0624a151727f8b2b3b70
Eugenia Carlini <eugeniacarlini@gmail.com>
2024-06-08 20:34:56 +0200

    feat: character components

---
+++ src/components/atoms/FavoriteIcon/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./styles.css";
+
+type FavoriteIconProps = {
+  isActive: boolean;
+};
+
+const FavoriteIcon: React.FC<FavoriteIconProps> = (isActive) => {
+  const iconModifier = isActive ? "red" : "empty";
+
+  return (
+    <svg
+      className={`icon__favorite icon__favorite--${iconModifier}`}
+      xmlns="http://www.w3.org/2000/svg"
+      width="13"
+      height="12"
+      fill="none"
+    >
+      <path
+        fillRule="evenodd"
+        strokeWidth="1"
+        d="M6.571 2.373l-3-1.82-3 1.82v3.902l6 5.115 6-5.115V2.373l-3-1.82-3 1.82z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+};
+
+export default FavoriteIcon;
